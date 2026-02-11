2002e1659a8a022206274cefefb9010f4aad68c6
Daniel Allen <dan@aeternagroup.net>
2026-02-11 23:55:43 +0000

    Update to schema version v4.59.0

--- variables.tf
+++ variables.tf
@@ -37,8 +37,8 @@ EOT
     name                   = string
     panorama_base64_config = string
     resource_group_name    = string
-    marketplace_offer_id   = optional(string, "pan_swfw_cloud_ngfw")
-    plan_id                = optional(string, "panw-cloud-ngfw-payg")
+    marketplace_offer_id   = optional(string) # Default: "pan_swfw_cloud_ngfw"
+    plan_id                = optional(string) # Default: "panw-cloud-ngfw-payg"
     tags                   = optional(map(string))
     network_profile = object({
       egress_nat_ip_address_ids = optional(list(string))
@@ -64,7 +64,7 @@ EOT
     }))
     dns_settings = optional(object({
       dns_servers   = optional(list(string))
-      use_azure_dns = optional(bool, false)
+      use_azure_dns = optional(bool) # Default: false
     }))
   }))
 }
